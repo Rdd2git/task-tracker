fb8321e0d8dfd17ea2a502fdb6612eb76c9a6d03
Radik Galiullin <rgaliullin55@gmail.com>
2025-04-21 16:05:18 +0500

    before update angular version

---
+++ src/app/models/task.model.ts
@@ -0,0 +1,19 @@
+export enum TaskStatus {
+  TODO = 'TODO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  status: TaskStatus;
+  priority: TaskPriority;
+  assignee: string;
+  deadline: Date;
+}
+export enum TaskPriority {
+  LOW = 'LOW',
+  MEDIUM = 'MEDIUM',
+  HIGH = 'HIGH',
+}
